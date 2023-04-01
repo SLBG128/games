b4a75e4fb1c6452d40019091abeec96df87d397c
SLBG128 <104144895+SLBG128@users.noreply.github.com>
2023-04-01 19:12:00 +0800

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,6 +2,13 @@ import random
 
 user_won_time = 0
 
+print("____________  _____   ")
+print("| ___ \ ___ \/  ___|  ")
+print("| |_/ / |_/ /\ `--.   ")
+print("|    /|  __/  `--. \  ")
+print("| |\ \| |    /\__/ /  ")
+print("\_| \_\_|    \____/   ")
+
 while True:                                                     ##########################################
     bot_choice = random.randint(1,3)                            # Let 1 = Rock , 2 = Paper , 3 = Scissors#
     print('You have won',user_won_time,'time(s)')               ##########################################
